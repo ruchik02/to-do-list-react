6970ceb3ca6353b4d588da19457aa6b064b0815b
ruchik02 <ruchika02>
2021-10-29 22:54:48 +0530

    to-do-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+import ToDoList from "./ToDoList";
 
-function App() {
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+  const delItem = (id) => {
+    console.log("deleted");
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElem,index)=>{
+          return index!==id;
+      })
+    })
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-div">
+        <div className="center-div">
+          <br />
+          <h1 className="heading">todo list</h1>
+          <br />
+          <input
+            type="text"
+            className="items"
+            placeholder="add items"
+            onChange={itemEvent}
+            value={inputList}
+          />
+          <button type="button" className="add-list" onClick={listOfItems}>
+            +
+          </button>
+          <ol>
+            {/* <li>{inputList}</li> */}
+            {items.map((itemVal, index) => {
+              return (
+                <ToDoList
+                  key={index}
+                  id={index}
+                  text={itemVal}
+                  onSelect={delItem}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
